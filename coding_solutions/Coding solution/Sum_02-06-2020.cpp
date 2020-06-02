441c9922843c438be57bfe346818e7293337a057
nayan1998 <nayan19985june@gmail.com>
2020-06-02 14:26:35 +0530

    02-06-2020

---
+++ coding_solutions/Coding solution/Sum_02-06-2020.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int no=147;
+	int no.res;
+	if(no)
+      res=no%9==0 ? 9:no %9;
+      else
+      res=0;
+      cout<<res;
+      return 0;
+}
+
+		
