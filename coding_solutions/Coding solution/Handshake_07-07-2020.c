4867002006de55beb47fc252939b9849e4763a8b
nayan1998 <nayan19985june@gmail.com>
2020-07-07 16:23:11 +0530

    07-07-2020

---
+++ coding_solutions/Coding solution/Handshake_07-07-2020.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+//fill the code
+	int num;
+	printf("enter the n value\n");
+	scanf("%d",&num);
+	int total = num * (num-1) / 2;
+	printf("the no of hand shake is\n")
+	printf("%d",total);
+	return 0;
+}
